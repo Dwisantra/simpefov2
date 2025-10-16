248f36abe39bbdfb767394ec8d82a91a38cddeb8
dwisantra <itrswiradadihusada@gmail.com>
2025-10-16 08:43:42 +0700

    title update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>{{ config('app.name') }} - Sistem Manajemen Pengajuan Form</title>
+    <title>{{ config('app.name') }} - Sistem Informasi Manajemen Pengajuan Form</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
